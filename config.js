e0cf4e8597eef30a7045a190888a37daa4eaf858
abadaan <73232889+abadaan@users.noreply.github.com>
2022-01-25 18:31:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,19 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB-QkAagMQd-RiswtMuSZfgjYifb02qALQ",
+  authDomain: "project-68-cf272.firebaseapp.com",
+  projectId: "project-68-cf272",
+  storageBucket: "project-68-cf272.appspot.com",
+  messagingSenderId: "1042556837892",
+  appId: "1:1042556837892:web:bfd78f54d97b31fc6b9551"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
